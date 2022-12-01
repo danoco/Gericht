40d81c14fd77729517b094c9763f9d79a97b3609
danoco <gzagz311@gmail.com>
2022-12-01 21:11:50 +0200

    gericht

---
+++ src/container/Findus/FindUs.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
+
+const FindUs = () => (
+  <div className='app__bg app__wrapper section__padding' id='contact'>
+    <div className='app__wrapper_info'>
+      <SubHeading title='Contact' />
+      <h2 className='headtext__cormorant' style={{ marginBottom: '64px' }}>
+        Find Us
+      </h2>
+      <div className='app__wrapper-content'>
+        <p className='p__opensans'>
+          Lane Ends Bungalow, Whatcroft Hall Lane, Rudheath, CW9 7SG
+        </p>
+        <p
+          className='p__cormorant'
+          style={{ color: '#DCCA87', margin: '16px 0' }}
+        >
+          Opening Hours
+        </p>
+        <p className='p__opensans' style={{ margin: '16px 0' }}>
+          Mon - Fri: 10:00 am - 02:00 am
+        </p>
+        <p className='p__opensans'>Sat - Sun: 10:00 am - 03:00 am</p>
+      </div>
+      <button className='custom__button' style={{ marginTop: '64px' }}>
+        Visit Us
+      </button>
+    </div>
+    <div className='app__wrapper_img'>
+      <img src={images.findus} alt='findus' />
+    </div>
+  </div>
+);
+
+export default FindUs;
